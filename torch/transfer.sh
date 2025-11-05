dd21ba919dc197a975e3031e6c4923cab00e6be3
HollyRiver <hcssk2800@gmail.com>
2025-11-05 15:52:11 +0900

    .

--- torch/transfer.sh
+++ torch/transfer.sh
@@ -1,4 +1,4 @@
-python trTST.py --model_num=100\
-                --data="coin"\
-                --lr=1e-6\
-                --backbone="PatchTSTBackbone"
+python trTFMLP.py --model_num=100\
+                  --data="coin"\
+                  --lr=1e-6\
+                  --backbone="PatchTSTBackbone"
